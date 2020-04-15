2a9a44718cfd8b9ee8f26ec6ff9d28f53008b7b8
Naoki_Desktop <naokigsyoshida@gmail.com>
2020-04-16 01:10:09 +0900

    石の出力

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ board_height=8
 board_width=8
 
 # GUI
-window_large=(640,360)
+window_large=(1280,720)
 title="オセロ-othello-"
 fps=30
 
@@ -14,6 +14,9 @@ black_color=(0,0,0)
 white_color=(255,255,255)
 
 GUIboard_origin=[5,5]
-GUIboard_height=350
-GUIboard_width=350
+GUIboard_height=700
+GUIboard_width=700
 line_thick=2
+
+grid_height=80
+grid_width=80
--- gui_othello.py
+++ gui_othello.py
@@ -24,8 +24,7 @@ class othello_GUI:
         # 色
         self.back_green=config.back_green
         self.line_color=config.line_color
-        self.black_color=config.black_color
-        self.white_color=config.white_color
+        self.stones_color={'B':config.black_color,'W':config.white_color}
 
         self.line_thick=config.line_thick
         # ピクセル的な高さと幅(GUI)
@@ -43,7 +42,6 @@ class othello_GUI:
         # ボードのマス的な高さと幅
         self.height_num=config.board_height
         self.width_num=config.board_width
-
     
     def draw_back(self):
         # ピクセル的な高さと幅(GUI)
@@ -62,6 +60,10 @@ class othello_GUI:
             add=i*width//width_num
             pygame.draw.line(self.screen,self.line_color,(origin[0]+add,origin[1]),(origin[0]+add,origin[1]+height),self.line_thick)
 
+    # color=str, position=(int,int,int), radius=int
+    def draw_stone(self,color,position,radius):
+        pygame.draw.circle(self.screen,self.stones_color[color],position,radius)
+
     def draw_stones(self):
         # ピクセル的な高さと幅(GUI)
         origin=self.GUIboard_origin
@@ -73,16 +75,16 @@ class othello_GUI:
 
         for i in range(height_num):
             for j in range(width_num):
-                color=None
-                if self.othello.board[i][j]:
-                    pass
-
-
+                # 半径は高さのみに依存(楕円にできるようにすべきか)
+                radius=height/height_num/2
+                if self.othello.board[i][j] in self.stones_color:
+                    self.draw_stone(self.othello.board[i][j],(int(origin[0]+radius*(1+2*i)),int(origin[1]+radius*(1+2*j))),int(radius*0.9))
 
     
     def run(self):
         while True:
             self.draw_back()
+            self.draw_stones()
             pygame.time.wait(config.fps)
             pygame.display.update()
             for event in pygame.event.get():
